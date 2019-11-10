87fb96234970a4965cb9cfe82d2398839e9eef65
Nattawut P <nata8ify@gmail.com>
2019-11-10 13:00:23 +0700

    Initial Commit

---
+++ iosgs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosgs
+//
+//  Created by Nattawut Pichaiwattnphon on 10/11/19.
+//  Copyright © 2019 Nattawut Pichaiwattnphon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
